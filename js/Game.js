44b5e3439383fdb7f533b2f95ca63f3d3ea761de
Indasia Summerfield <Indasia@yahoo.com>
2019-02-22 11:08:21 -0800

    Added to to Phrase.js and Game.js

--- js/Game.js
+++ js/Game.js
@@ -3,15 +3,35 @@
  * Game.js */
 
 class Game {
-    constructor(missed, phrases, activePhrase) {
-        this.missed = missed;
-        this.phrases = phrases;
-        this.activePhrase = activePhrase;
+    constructor() {
+        // used to track the number of missed guesses by the player. initial value = 0
+        this.missed = 0;
+        // an array of Phrase objects to use with the game
+        this.phrases = this.createPhrases(); 
+        // the Phrase object currently in play. initial value = null
+        this.activePhrase = null;
     }
-    startGame(){}
-    getRandomPhrase(){}
-    getRandomPhrase(){}
-    removeLife(){}
-    checkForWin(){}
-    gameOver(){}
+
+    // creates phrases for use in game and returns it
+    createPhrases() { 
+        let phraseArray = [
+            new Phrase("Stay weird."),
+            new Phrase("Live a life of purpose."),
+            new Phrase("True happiness lies within."),
+            new Phrase("Namaste."),
+            new Phrase("Let good things happen.")
+            ];
+        return phraseArray;
+    };   
+
+    // selects random phrase from phrases property and returns it
+    getRandomPhrase() {
+        let randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase;       
+    }
+
+    //startGame(){}
+    //removeLife(){}
+    //checkForWin(){}
+    //gameOver(){} 
 }
--- js/Phrase.js
+++ js/Phrase.js
@@ -4,9 +4,30 @@
 
 class Phrase {
     constructor(phrase) {
-        this.phrase = phrase;
+        // the actual phrase the Phrase object is representing
+        this.phrase = phrase.toLowerCase();
     }
-    addPhraseToDisplay(){}
-    checkLetter(){}
-    showMatchedLetter(){}
- }
+}
+
+    // adds letter placeholders to the display and displays phrase on game board
+    addPhraseToDisplay(){
+       // select the ul element
+        const ul = document.querySelector("#phrase ul");
+        let splitPhrase = this.phrase.split("");
+        // for each letter in the selected array, create an li
+        splitPhrase.forEach(letter => {
+            let li = document.createElement("li");
+            // if the letter is a space, concatenate the list item with a space
+            if (letter === " ") {
+                li.innerHTML = "<li>" + " " + "</li>";
+            // otherwise, concatenate the html for a list item with a character
+            } else {
+                li.innerHTML = "<li>" + ${letter} + "</li>";
+            }
+            });
+    ul.appendChild(li);
+};
+        
+   // checkLetter(){}
+   // showMatchedLetter(){}
+ 
